aa823669fba8966831ab15f28736b9ad19c0bdd3
Tsaqif <mushoddaqt@gmail.com>
2023-06-28 18:24:34 +0700

    fix post method to detail

--- app/routes/detailRoute.js
+++ app/routes/detailRoute.js
@@ -2,7 +2,7 @@ import express from "express";
 import { detailIkan, allIkan } from "../controller/detailController.js";
 const router = new express.Router();
 
-router.get("/detail", detailIkan);
+router.post("/detail", detailIkan);
 router.get("/all", allIkan);
 
 export default router;
